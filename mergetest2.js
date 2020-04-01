a99a4ca38ca29fa01aa78c78a3c062d283319dea
HirudineeADRT <hirudinee@adroitlogic.com>
2020-04-01 18:30:41 +0530

    hiru

---
+++ mergetest2.js
@@ -0,0 +1,27 @@
+let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
+const cognito_idp = new AWS.CognitoIdentityServiceProvider();
+
+exports.handler = async (event) => {
+    try {
+        let data = await cognito_idp.listUsers({
+            UserPoolId: process.env.UserPoolId_cognitol,
+            Limit: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+    try {
+        let data = await s3.listObjects({
+            Bucket: "as2-test-lahiru",
+            MaxKeys: 10
+        }).promise();
+
+    } catch (err) {
+        // error handling goes here
+    };
+
+
+    return { "message": "Successfully executed" };
+};
